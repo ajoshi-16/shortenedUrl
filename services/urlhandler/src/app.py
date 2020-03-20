74d4c61b6e6fafe851831d01d1fb6ffdc9c08a98
Anuvrath <anuvrath.joshi@gmail.com>
2020-03-19 19:00:13 -0700

    Change

--- services/urlhandler/src/app.py
+++ services/urlhandler/src/app.py
@@ -8,3 +8,12 @@ from bson.json_util import dumps
 __author__ = "Anuvrath Joshi"
 
 createUrl_blueprint = Blueprint('aboutauto', __name__, template_folder='templates', static_folder='static')
+
+
+@createUrl_blueprint.route('/urlhandler/createshorturl', methods=['POST'])
+def create_url():
+    """
+    This service handles the add URL requests
+    """
+
+    return jsonify({'Status': 'Successfully Edited the User'}), 200
